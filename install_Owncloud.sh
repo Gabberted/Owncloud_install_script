3c63fca0741999b85c7804b65971db1af926f08c
Gabberted <mail@rakesh.info>
2022-02-06 18:28:02 +0100

    chmod +x on the install script

--- install_Owncloud.sh
+++ install_Owncloud.sh
@@ -1,8 +1,8 @@
-sudo apt-get install apache2
-sudo apt-get install php5 php5-gd sqlite php5-sqlite php5-curl
+sudo apt install apache2
+sudo apt install php5 php5-gd sqlite php5-sqlite php5-curl
 sudo service apache2 restart
-wget https://download.owncloud.org/community/owncloud-*.zip
-sudo mv owncloud-*.zip /var/www/html
+wget https://download.owncloud.org/community/daily/owncloud-daily-master.zip
+sudo mv owncloud-daily-master.zip /var/www/html
 cd /var/www/html
 sudo unzip -q owncloud-*.zip
 sudo mkdir /var/www/html/owncloud/data
