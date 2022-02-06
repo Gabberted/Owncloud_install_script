7ad738aecb805e21a28a2b752e0e7a6a6586189d
Gabberted <mail@rakesh.info>
2022-02-06 19:04:45 +0100

    Updated to latest version

--- install_Owncloud.sh
+++ install_Owncloud.sh
@@ -1,10 +1,11 @@
 sudo apt install apache2
-sudo apt install php5 php5-gd sqlite php5-sqlite php5-curl
+sudo apt install php7.4 php7.4-gd sqlite3 php7.4-sqlite3 php7.4-curl php7.4-zip php7.4-intl php7.4-xml php-mbstring
 sudo service apache2 restart
-wget https://download.owncloud.org/community/daily/owncloud-daily-master.zip
-sudo mv owncloud-daily-master.zip /var/www/html
+wget https://download.owncloud.org/community/owncloud-complete-latest.zip
+sudo mv owncloud-complete-latest.zip /var/www/html
 cd /var/www/html
-sudo unzip -q owncloud-*.zip
+sudo unzip -q owncloud-complete-latest.zip
 sudo mkdir /var/www/html/owncloud/data
 sudo chown www-data:www-data /var/www/html/owncloud/data
 sudo chmod 750 /var/www/html/owncloud/data
+echo "finished"
